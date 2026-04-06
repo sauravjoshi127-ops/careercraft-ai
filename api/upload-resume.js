885e242290e61e1b79dba1611415b202de889291
Saurav joshi <sauravjoshi127@gmail.com>
2026-04-06 15:31:28 +0530

    Replace pdf-parse with PDFParse for PDF handling
    Refactor PDF parsing to use PDFParse class

--- api/upload-resume.js
+++ api/upload-resume.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const Busboy = require('busboy');
-const pdfParse = require('pdf-parse/lib/pdf-parse.js');
+const { PDFParse } = require('pdf-parse');
 const mammoth = require('mammoth');
 
 const ALLOWED_MIME_TYPES = [
@@ -105,7 +105,9 @@ module.exports = async function handler(req, res) {
 
     if (isPDF) {
       console.log('[upload] Parsing PDF…');
-      const parsed = await pdfParse(resumeFile.buffer);
+      const parser = new PDFParse({ data: resumeFile.buffer });
+      const parsed = await parser.getText();
+      await parser.destroy();
       resumeText = parsed.text || '';
     } else if (isDOCX) {
       console.log('[upload] Parsing DOCX…');
